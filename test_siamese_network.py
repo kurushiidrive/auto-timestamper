89ddb439a4202479456997ae0bc0a64afad1a8f9
kurushiidrive <kurushiidrive@gmail.com>
2021-08-19 21:33:02 -0700

    Test siamese neural network w/ montage visual

--- test_siamese_network.py
+++ test_siamese_network.py
@@ -3,6 +3,7 @@ import numpy as np
 import utils
 import config
 from tensorflow import keras
+from imutils import build_montages
 from siamese_network import build_siamese_model
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense
@@ -24,7 +25,7 @@ X_test = tmpX / 255.0
 y_test = np.array(tmpY)
 
 (pair_test, label_test) = utils.make_pairs(X_test, y_test)
-
+(tmp_pair_test, tmp_label_test) = utils.make_pairs(tmpX, y_test)
 
 # rebuild model
 imgA = Input(shape=config.IMG_SHAPE)
@@ -47,3 +48,34 @@ x_test_2 = pair_test[:, 1]
 
 results = model.evaluate([x_test_1, x_test_2], label_test)
 print("test loss, test acc:", results)
+
+predictions = model.predict([x_test_1, x_test_2])
+print(predictions)
+print(predictions.shape)
+
+images = []
+
+for i in range(predictions.shape[0]):
+    imageA = tmp_pair_test[i][0]
+    imageB = tmp_pair_test[i][1]
+#    label = tmp_label_train[i]
+    
+    # padding for easier visualization
+#    output = np.zeros((36, 60), dtype="uint8")
+    pair = np.hstack([imageA, imageB])
+#    output[4:32, 0:56] = pair
+    
+    text = "neg" if predictions[i][0] < 0.5 else "pos"
+    color = (0, 0, 255) if predictions[i][0] < 0.5 else (0, 255, 0)
+    
+#    vis = cv.merge([output] * 3)    # 3-channel RGB image from original grayscale
+    vis = cv.resize(pair, (96, 51), interpolation=cv.INTER_LINEAR)
+    cv.putText(vis, text, (2, 12), cv.FONT_HERSHEY_SIMPLEX, 0.75, color, 2)
+    
+    images.append(vis)
+    
+montage = build_montages(images, (96, 51), (7, 7))[0]
+
+cv.imshow("Siamese Image Pairs", montage)
+while cv.waitKey(0) != ord('q'):
+    pass
